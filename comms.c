3f26953c1745ae2bbac3b46400cf7114f9e29da6
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2018-10-11 12:42:34 +1300

    Commenting comms.c

--- comms.c
+++ comms.c
@@ -14,10 +14,14 @@ void comms_init(void) {
 * Sends a code to start the game to the other board
 */
 uint8_t send_start(void) {
+    // Checking if ready to send
     if (ir_uart_write_ready_p()) {
+        // Sending START_CODE
         ir_uart_putc(START_CODE);
+        // Returning 1 to indicate success
         return 1;
     } else {
+        // Send failed so returning 0
         return 0;
     }
 }
@@ -27,10 +31,14 @@ uint8_t send_start(void) {
 * Sending a code to the other board informing it that it won the game
 */
 uint8_t send_won(void) {
+    // Checking if ready to send
     if (ir_uart_write_ready_p()) {
+        // Sending WIN_CODE
         ir_uart_putc(WIN_CODE);
+        // Returning 1 to indicate success
         return 1;
     } else {
+        // Send failed so returning 0
         return 0;
     }
 }
@@ -40,11 +48,15 @@ uint8_t send_won(void) {
 * Sends the ball to the other board
 */
 uint8_t send_ball(Ball ball) {
+    // Checking if ready to send
     if (ir_uart_write_ready_p()) {
+        // Sending a BALL_CODE
         ir_uart_putc(BALL_CODE);
         //TODO Need to send ball postion here
+        // Returning 1 to indicate success
         return 1;
     } else {
+        // Send failed so returning 0
         return 0;
     }
 }
@@ -54,21 +66,24 @@ uint8_t send_ball(Ball ball) {
 *
 */
 Data receiveData(void) {
+    // Initializing variables used to receive data
     uint8_t message;
     Data dataReceived = {EMPTY, 0};
 
+    // Checking if ready to receive
     if (ir_uart_read_ready_p) {
-        message = ir_uart_getc();
+        // Receiving data into the dataReceived variable
+        dataReceived.data_type = ir_uart_getc();
 
+        // A win code has been recived
         if (message = WIN_CODE) {
-            dataReceived.data_type = message;
             dataReceived.ball = ir_uart_getc();
+        // Ball has been received
         } else if (message = BALL_CODE) {
             dataReceived.data_type = message;
-
-        } else if (message = START_CODE) {
-            dataReceived.data_type = message;
         }
+        // Else either a START_CODE or in valid message has been received
     }
+    // Returning the data that was received
     return dataReceived;
 }
